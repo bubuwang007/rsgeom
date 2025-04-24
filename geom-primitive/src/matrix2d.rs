3f4eb53625f6d3df841a24c2c306a766431f569f
bubuwang007 <1191651771@qq.com>
2025-04-24 16:50:47 +0800

    2025.4.24 16:50

--- geom-primitive/src/matrix2d.rs
+++ geom-primitive/src/matrix2d.rs
@@ -3,6 +3,8 @@ pub struct Matrix2d {
     m: [[f64; 2]; 2],
 }
 
+use crate::xy::XY;
+
 impl Matrix2d {
     pub fn new() -> Self {
         Matrix2d { m: [[0.0; 2]; 2] }
@@ -22,8 +24,16 @@ impl Matrix2d {
         [self.m[0][index], self.m[1][index]]
     }
 
+    pub fn col_xy(&self, index: usize) -> XY {
+        XY::from_coordinates(self.m[0][index], self.m[1][index])
+    }
+
     pub fn row(&self, index: usize) -> [f64; 2] {
         self.m[index]
+    }
+
+    pub fn row_xy(&self, index: usize) -> XY {
+        XY::from_coordinates(self.m[index][0], self.m[index][1])
     }
 
     pub fn set_col(&mut self, index: usize, col: [f64; 2]) {
@@ -31,17 +41,47 @@ impl Matrix2d {
         self.m[1][index] = col[1];
     }
 
+    pub fn set_col_xy(&mut self, index: usize, xy: XY) {
+        self.m[0][index] = xy.x;
+        self.m[1][index] = xy.y;
+    }
+
     pub fn set_row(&mut self, index: usize, row: [f64; 2]) {
         self.m[index] = row;
+    }
+
+    pub fn set_row_xy(&mut self, index: usize, xy: XY) {
+        self.m[index][0] = xy.x;
+        self.m[index][1] = xy.y;
+    }
+
+    pub fn set_matrix(&mut self, m: [[f64; 2]; 2]) {
+        self.m = m;
+    }
+
+    pub fn set_matrix_xy(&mut self, m: [XY; 2]) {
+        self.m[0][0] = m[0].x;
+        self.m[0][1] = m[0].y;
+        self.m[1][0] = m[1].x;
+        self.m[1][1] = m[1].y;
     }
 
     pub fn diagonal(&self) -> [f64; 2] {
         [self.m[0][0], self.m[1][1]]
     }
 
+    pub fn diagonal_xy(&self) -> XY {
+        XY::from_coordinates(self.m[0][0], self.m[1][1])
+    }
+
     pub fn set_diagonal(&mut self, diag: [f64; 2]) {
         self.m[0][0] = diag[0];
         self.m[1][1] = diag[1];
+    }
+
+    pub fn set_diagonal_xy(&mut self, diag: XY) {
+        self.m[0][0] = diag.x;
+        self.m[1][1] = diag.y;
     }
 
     pub fn set_identity(&mut self) {
@@ -58,6 +98,29 @@ impl Matrix2d {
         )
     }
 
+    pub fn set_rotation(&mut self, angle: f64) {
+        let cos_angle = angle.cos();
+        let sin_angle = angle.sin();
+        self.m[0][0] = cos_angle;
+        self.m[0][1] = -sin_angle;
+        self.m[1][0] = sin_angle;
+        self.m[1][1] = cos_angle;
+    }
+
+    pub fn set_scale(&mut self, scale: f64) {
+        self.m[0][0] = scale;
+        self.m[1][1] = scale;
+        self.m[0][1] = 0.0;
+        self.m[1][0] = 0.0;
+    }
+
+    pub fn determinant(&self) -> f64 {
+        self.m[0][0] * self.m[1][1] - self.m[0][1] * self.m[1][0]
+    }
+
+    pub fn is_singular(&self) -> bool {
+        self.determinant().abs() < f64::MIN_POSITIVE
+    }
 
 }
 
--- geom-primitive/tests/test_matrix2d.rs
+++ geom-primitive/tests/test_matrix2d.rs
@@ -30,4 +30,51 @@ mod tests {
         assert_eq!(m4[1][0], 3.0);
         assert_eq!(m4[1][1], 4.0);
     }
+
+    #[test]
+    fn test_xy() {
+        let m = Matrix2d::from_coordinates(1.0, 2.0, 3.0, 4.0);
+        let xy1 = m.col_xy(0);
+        let xy2 = m.row_xy(1);
+        assert_eq!(xy1.x, 1.0);
+        assert_eq!(xy1.y, 3.0);
+        assert_eq!(xy2.x, 3.0);
+        assert_eq!(xy2.y, 4.0);
+    }
+
+    #[test]
+    fn test_diagonal() {
+        let mut m = Matrix2d::new();
+        m.set_diagonal([5.0, 6.0]);
+        assert_eq!(m.diagonal(), [5.0, 6.0]);
+        assert_eq!(m[0][0], 5.0);
+        assert_eq!(m[1][1], 6.0);
+    }
+
+    #[test]
+    fn test_identity() {
+        let mut m = Matrix2d::new();
+        m.set_identity();
+        assert_eq!(m[0][0], 1.0);
+        assert_eq!(m[1][1], 1.0);
+        assert_eq!(m[0][1], 0.0);
+        assert_eq!(m[1][0], 0.0);
+    }
+
+    #[test]
+    fn test_rotation() {
+        let mut m = Matrix2d::new();
+        m.set_rotation(45_f64.to_radians());
+        assert!((m[0][0] - 0.7071).abs() < 0.0001);
+        assert!((m[0][1] + 0.7071).abs() < 0.0001);
+        assert!((m[1][0] - 0.7071).abs() < 0.0001);
+        assert!((m[1][1] - 0.7071).abs() < 0.0001);
+    }
+
+    #[test]
+    fn test_determinant() {
+        let m = Matrix2d::from_coordinates(1.0, 2.0, 3.0, 4.0);
+        assert_eq!(m.determinant(), -2.0);
+    }
+
 }
